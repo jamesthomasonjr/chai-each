645528b84cb8019213bf7d6e6ce83d00ae712118
JT <jamesthomason@quickenloans.com>
2018-11-05 07:30:46 -0500

    Add new tests

--- test/chai-each.test.js
+++ test/chai-each.test.js
@@ -1,4 +1,5 @@
 const regularArray = [1,2,3,4,5];
+const objectArray = [{"value": 4}, {"value": 11}, {"value": 15}];
 const nestedArray = [
     [1,2,3],
     [4,5,6],
@@ -13,7 +14,17 @@ describe("chai-each", () => {
     it("should fail correctly", () => {
         const assertion = () => expect(regularArray).to.each.be.at.least(10);
 
-        expect(assertion).to.throw("expected 1 to be at least 10");
+        expect(assertion).to.throw(/expected \d to be at least 10/);
+    });
+
+    it("should succeed correctly with property", () => {
+        expect(objectArray).to.each.have.property('value').that.is.at.most(20)
+    });
+
+    it("should fail correctly with property", () => {
+        const assertion = () => expect(objectArray).to.each.have.property('value').that.is.at.least(20);
+
+        expect(assertion).to.throw(/expected \d{1,2} to be at least 20/);
     });
 
     it.skip("@TODO in v2: should succeed correctly with nested arrays", () => {
@@ -23,6 +34,6 @@ describe("chai-each", () => {
     it.skip("@TODO in v2: should fail correctly with nested arrays", () => {
         const assertion = () => expect(nestedArray).to.each.be.an('array').with.each.at.least(10);
 
-        expect(assertion).to.throw("expected 1 to be at least 10");
+        expect(assertion).to.throw(/expected \d to be at least 10/);
     });
 });
